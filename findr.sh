b1e140ff6d94b1783f3c50a755b0131b819d754d
Scott Carpenter <scarpent@nerdery.com>
2014-11-03 05:13:10 -0600

    remove -e and -c options in favor of using fcp bash function to work with ~/.findr_results

--- findr.sh
+++ findr.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
  usage="\nfindr.sh: Helper to make regex find commands easier.\n\n"
-usage+="  usage: findr.sh [-a] [-ec] [-n] [-p] [-s] [find args] expression\n\n"
+usage+="  usage: findr.sh [-a] [-n] [-p] [-s] [find args] expression\n\n"
 usage+="\t-a\tsearch all filesystems (default is -xdev/-mount)\n"
 usage+="\t-n\tadd line numbers to results\n"
 usage+="\t-p\tpreview mode; just show the resulting find command\n"
 usage+="\t-s\tsave results to ~/.findr_results (w/o line numbers)\n"
 
-usage+="\n\t\tyou can't combine params as you do, e.g. -ap,"
-usage+="\n\t\t*except* for in the funny business to follow:\n"
+usage+="\n\t\tyou can't combine params as you do, e.g. -ap\n"
 
-usage+="\n\t-cN\tcopy only line number N of results to clipboard;\n"
-usage+="\t\tusing mac osx pbcopy\n"
-usage+="\t-eN\techo only line number N of results\n"
-
-usage+="\n\t\t-c copy or -e echo only line number N of results,\n"
-usage+="\t\tfrom ~/.findr_results if it exists and no regex is\n"
-usage+="\t\tprovided, otherwise from a new search\n"
-
-usage+="\n\t\tyou can combine these params, e.g. -ec2, but -c will\n"
-usage+="\t\talways echo so that the e is unnecessary; N must be\n"
-usage+="\t\tadded with no preceding space, but can be ommitted\n"
-usage+="\t\tfor a default value of 1\n"
-
-usage+="\n\t\t-c or -e will suppress line numbers and saving to\n"
- usage+="\t\t~/.findr_results, even if -n or -s is specified\n"
+# use line numbering and saving to ~/.findr_results with fcp bash function
+# and: alias findr='findr.sh -n -s'
 
 preview=false
 # use "nowarn" to avoid warning, e.g. if -mount used after -type f
@@ -49,17 +35,6 @@ while [ -n "$1" ]; do
         preview=true
     elif [[ "$1" == "-s" ]]; then
         save_results=true
-    elif [[ "$1" =~ ^-(e?c|ce?|ec?|c?e)([0-9]+)?$ ]]; then
-        if [[ "$1" =~ e ]]; then
-            line_echo=true
-        fi
-        if [[ "$1" =~ c ]]; then
-            line_to_clipboard=true
-        fi
-        line_number=$(echo "$1" | sed 's/[^0-9]//g')
-        if [[ "$line_number" =~ ^$|0+ ]]; then
-            line_number=1
-        fi
     elif [[ $# -eq 1 ]]; then
         regex=$1
     else
@@ -68,34 +43,12 @@ while [ -n "$1" ]; do
     shift
 done
 
-function echo_copy_result {
-    echo $result
-    if [[ "$line_to_clipboard" == "true" ]]; then
-        # -n to suppress newline
-        echo -n "$result" | pbcopy
-    fi
-}
-
-if [[ "$line_echo" == "true" || "$line_to_clipboard" == "true" ]]; then
-    # printf to suppress newline when copying to clipboard
-    awk_param="NR==$line_number {printf}"
-
-    if [[ -e "$results_file" && "$regex" == "" ]]; then
-        result=$(awk "$awk_param" "$results_file")
-        echo_copy_result
-        exit 0
-    fi
-fi
-
 # reminder: adding $regex to $args didn't seem to work right
 args+="${xdev}-regextype posix-egrep -iregex"
 regex="^.*${regex}[^/]*$"
 
 if [[ "$preview" == "true" ]]; then
     echo "find $args ""'""$regex""'" >&2
-elif [[ "$line_echo" == "true" || "$line_to_clipboard" == "true" ]]; then
-    result=$(find $args "$regex" | awk "$awk_param")
-    echo_copy_result
 elif [[ "$print_line_numbers" == "false" && "$save_results" == "false" ]]; then
     find $args "$regex"
 elif [[ "$print_line_numbers" == "true" && "$save_results" == "false" ]]; then
